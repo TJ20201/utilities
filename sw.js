dc804d46a5bf42539785c420a3ccafa3620267c6
TJ <86313252+TJ20201@users.noreply.github.com>
2022-06-25 14:10:31 +0100

    Create service worker JS

---
+++ sw.js
@@ -0,0 +1,7 @@
+var GHPATH = '/utilities'
+var APP_PREFIX = 'utilities_'
+var VERSION = 'version_00'
+var URLS = [
+ '${GHPATH}/index.html','${GHPATH}/index.js','${GHPATH}/index.css',
+ '${GHPATH}/todolist/todolist.html','${GHPATH}/todolist/todolist.js','${GHPATH}/todolist/todolist.css',
+]
