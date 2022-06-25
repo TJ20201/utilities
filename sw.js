ef9babc4ea33715fc67d0dacaaa346be5b389ce7
TJ20201 <86313252+TJ20201@users.noreply.github.com>
2022-06-25 14:57:35 +0100

    Update PWA Version from 00 to 01

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 var GHPATH = '/utilities'
 var APP_PREFIX = 'utilities_'
-var VERSION = 'version_00'
+var VERSION = 'version_01'
 var URLS = [
  '${GHPATH}/index.html','${GHPATH}/index.js','${GHPATH}/index.css',
  '${GHPATH}/todolist/todolist.html','${GHPATH}/todolist/todolist.js','${GHPATH}/todolist/todolist.css',
